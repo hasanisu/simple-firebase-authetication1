2620cae6eeb84c45e0716071a5d02eff06cdcd56
hasanisu <hasanjimam@gmail.com>
2023-02-28 18:07:05 +0900

    first commit

---
+++ _steps.js
@@ -0,0 +1,23 @@
+/*
+...............................
+INITIAL SETUP
+...............................
+1. visit: <console className="firebase google.com
+2. create a new firebase project 
+3. visit doc (go to docs): Build > Authentication > web > Getting started
+4. Register web app > firebase project home > click Web> give naem and register
+5. install firebase for your project: install firebase 
+6. Dangerous: get firebase config andput it in firebase.init.js 
+7.expost app from firebase.init.js 
+............................
+SETUP THE PROVIDER 
+............................
+8. create auth using getAuth from firebase by using app firm firebase.init.js 
+9. create a google auth provider. do not forget to use new GoogleAuthProvider(); 
+10. go to firebase > build > authentication > sing in method
+11. Enable google sing in method
+12. create a button for google sing in method with a click handler.
+13. inside the event handler, call signInWithPopup with auth, provider.
+14. after singInWithPopup .then result, error
+
+*/
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVIuXn4vZ0dx-MwVRNJ35VCGT1sRIXXjE",
+  authDomain: "simpl-firebase-authentication1.firebaseapp.com",
+  projectId: "simpl-firebase-authentication1",
+  storageBucket: "simpl-firebase-authentication1.appspot.com",
+  messagingSenderId: "469767109926",
+  appId: "1:469767109926:web:cc2aa0869d6c10115495e7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
